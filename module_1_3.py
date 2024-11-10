af051076358cb8cd0d4cd36bfccd441a45acf6ec
Viktori <viktoria4283@gmail.com>
2024-11-10 13:34:02 +0400

    Create 1.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,5 +1,8 @@
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
+name = 'Viktoria'
+print('Name:', name)
+age = 17
+print('Age:', age)
+age = 17 + 1
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
